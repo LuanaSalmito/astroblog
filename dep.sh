1204eba8b8f94c40bd1fd3a5b9d0c2e5d72d083b
LuanaSalmito <luasalmito@gmail.com>
2023-12-05 13:57:39 -0300

    ok

---
+++ dep.sh
@@ -0,0 +1,39 @@
+
+#!/bin/bash
+
+npm run build
+
+git add .
+git commit -m "ok"
+git push
+
+
+if git checkout deploy; then
+    git rm -r .
+    git checkout -f main build
+    mv build/* .
+	echo "node_modules/" > .gitignore
+    git add .
+    git commit -m "ok"
+    git push origin deploy
+
+    # Create .htaccess file
+    echo "<IfModule mod_rewrite.c>
+  RewriteEngine On
+  RewriteBase /
+  RewriteRule ^index\.html$ - [L]
+  RewriteCond %{REQUEST_FILENAME} !-f
+  RewriteCond %{REQUEST_FILENAME} !-d
+  RewriteCond %{REQUEST_FILENAME} !-l
+  RewriteRule . /index.html [L]
+</IfModule>
+" > .htaccess
+
+    git add .htaccess
+    git commit -m "Add .htaccess file"
+    git push origin deploy
+
+    git checkout main
+else
+    echo "Checkout to 'dep' branch failed."
+fi
